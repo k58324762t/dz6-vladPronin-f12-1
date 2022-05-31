368255c19a401360c02022d5dfaf80dea3a7dd46
k58324762t <wirdjsgt@gmail.com>
2022-05-31 22:11:18 +0600

    master

---
+++ src/page/mainpage/MainPage.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react'
+import { Users } from '../../componets/User/Users'
+
+const URl = 'https://jsonplaceholder.typicode.com/users'
+
+function MainPage() {
+	const [data, setData] = useState([])
+
+	useEffect(() => {
+		fetch(URl)
+			.then(res => res.json())
+			.then(data => setData(data))
+	})
+
+	return (
+		<>
+			<table>
+				<tr>
+					<td>Name</td>
+					<td>email</td>
+					<td>userName</td>
+				</tr>
+				<tr>
+					<Users data={data} />
+				</tr>
+			</table>
+		</>
+	)
+}
+export default MainPage
